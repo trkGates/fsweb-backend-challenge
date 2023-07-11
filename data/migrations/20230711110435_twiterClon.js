89e6732f7ecda19967ae42a463953ad3d7511d0d
Yusuf Çolak <yusuf21706@gmail.com>
2023-07-11 23:18:43 +0300

    databashe oluşturuldu

--- data/migrations/20230711110435_twiterClon.js
+++ data/migrations/20230711110435_twiterClon.js
@@ -3,18 +3,53 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-    return knex.schema.createTable('users', function (table) {
-      table.increments('id').primary();
-      table.string('username').notNullable();
-      table.string('email').notNullable();
-      table.timestamps(true, true);
+  return knex.schema
+    .createTable("users", function (table) {
+      table.increments("user_id");
+      table.string("username", 255).notNullable();
+      table.string("password", 255).notNullable();
+      table.string("email", 255).notNullable();
+      table.string("avatar", 255).notNullable();
+      table.timestamp("tarih").defaultTo(knex.fn.now());
+    })
+    .createTable("posts", function (table) {
+      table.increments("post_id");
+      table.integer("user_id").unsigned().notNullable();
+      table.foreign("user_id").references("user_id").inTable("users");
+      table.string("content", 255).notNullable();
+      table.integer("like_sayisi").notNullable();
+      table.timestamp("tarih").defaultTo(knex.fn.now());
+    })
+    .createTable("comments", function (table) {
+      table.increments("comment_id");
+      table.integer("user_id").unsigned().notNullable();
+      table.foreign("user_id").references("user_id").inTable("users");
+      table.integer("post_id").unsigned().notNullable();
+      table.foreign("post_id").references("post_id").inTable("posts");
+      table.string("content", 255).notNullable();
+      table.integer("like_sayisi").notNullable();
+      table.timestamp("tarih").defaultTo(knex.fn.now());
+    })
+    .createTable("likes", function (table) {
+      table.increments("like_id");
+      table.integer("user_id").unsigned().notNullable();
+      table.foreign("user_id").references("user_id").inTable("users");
+      table.integer("post_id").unsigned().notNullable();
+      table.foreign("post_id").references("post_id").inTable("posts");
+      table.integer("comment_id").unsigned().notNullable();
+      table.foreign("comment_id").references("comment_id").inTable("comments");
+      table.timestamp("tarih").defaultTo(knex.fn.now());
     });
-  };
+};
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-    return knex.schema.dropTable('users');
-  };
+  return knex.schema
+    .dropTable("likes")
+    .dropTable("comments")
+    .dropTable("posts")
+    .dropTable("users");
+};
--- data/seeds/01_twiterClon.js
+++ data/seeds/01_twiterClon.js
@@ -1,13 +1,108 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
- */
-exports.seed = function (knex) {
-  return knex('users').del()
-    .then(function () {
-      return knex('users').insert([
-        { username: 'john', email: 'john@example.com' },
-        { username: 'emma', email: 'emma@example.com' }
-      ]);
-    });
+exports.seed = async function (knex) {
+  await knex("users").del();
+  await knex("posts").del();
+  await knex("comments").del();
+  await knex("likes").del();
+
+  await knex("users").insert([
+    {
+      user_id: 1,
+      username: "trkGates",
+      password: "123456",
+      email: "trkGates@gmail.com",
+      avatar: "https://picsum.photos/200/200",
+      tarih: new Date(),
+    },
+    {
+      user_id: 2,
+      username: "Workintech",
+      password: "password2",
+      email: "Workintech@gmail.com",
+      avatar: "https://picsum.photos/200/200",
+      tarih: new Date(),
+    },
+    {
+      user_id: 3,
+      username: "BillGates",
+      password: "password3",
+      email: "BillGates@gmail.com",
+      avatar: "https://picsum.photos/200/200",
+      tarih: new Date(),
+    },
+  ]);
+ 
+  await knex("posts").insert([
+    {
+      post_id: 1,
+      user_id: 1,
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae.",
+      like_sayisi: 0,
+      tarih: new Date(),
+    },
+    {
+      post_id: 2,
+      user_id: 2,
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae.",
+      like_sayisi: 0,
+      tarih: new Date(),
+    },
+    {
+      post_id: 3,
+      user_id: 3,
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae.",
+      like_sayisi: 0,
+      tarih: new Date(),
+    },
+  ]);
+
+  await knex("comments").insert([
+    {
+      comment_id: 1,
+      post_id: 1,
+      user_id: 1,
+      content: "Sana Katılmıyorum :)",
+      like_sayisi: 0,
+      tarih: new Date(),
+    },
+    {
+      comment_id: 2,
+      post_id: 2,
+      user_id: 2,
+      content:"Çok Haklısın :)",
+      like_sayisi: 0,
+      tarih: new Date(),
+    },
+    {
+      comment_id: 3,
+      post_id: 3,
+      user_id: 3,
+      content: "Kesinlikle Katılıyorum",
+      like_sayisi: 0,
+      tarih: new Date(),
+    },
+  ]);
+     
+  await knex("likes").insert([
+    {
+      like_id: 1,
+      post_id: 1,
+      user_id: 1,
+      comment_id: 1,
+      tarih: new Date(),
+    },
+    {
+      like_id: 2,
+      post_id: 2,
+      user_id: 2,
+      comment_id: 2,
+      tarih: new Date(),
+    },
+    {
+      like_id: 3,
+      post_id: 3,
+      user_id: 3,
+      comment_id: 3,
+      tarih: new Date(),
+    },
+  ]);
 };
